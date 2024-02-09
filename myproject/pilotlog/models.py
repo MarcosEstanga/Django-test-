4964ce66d9cf89d486ca127eeb45febb65856bad
MarcosEstanga <114589850+MarcosEstanga@users.noreply.github.com>
2024-02-09 16:26:28 -0400

    Update models.py

--- myproject/pilotlog/models.py
+++ myproject/pilotlog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class SettingConfigQuerySet(models.QuerySet):
+    def with_meta_info(self):
+        """Filters setting configurations that have metadata information."""
+        return self.exclude(meta={})
+
 class SettingConfig(models.Model):
     user_id = models.IntegerField()
     table = models.CharField(max_length=100)
@@ -8,7 +13,7 @@ class SettingConfig(models.Model):
     platform = models.CharField(max_length=100)
     modified = models.DateTimeField(auto_now=True)
 
-    objects = models.Manager()  # Default manager
+     objects = models.Manager.from_queryset(SettingConfigQuerySet)()
 
     def __str__(self):
         return f"{self.user_id} - {self.table}"
