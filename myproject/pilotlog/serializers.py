93d3692128f7ec14f35d06d07451d9cc53fdc26a
MarcosEstanga <114589850+MarcosEstanga@users.noreply.github.com>
2024-02-09 16:40:02 -0400

    Update serializers.py

--- myproject/pilotlog/serializers.py
+++ myproject/pilotlog/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from drf_dynamic_fields import DynamicFieldsMixin
 from .models import SettingConfig
 
-class SettingConfigSerializer(serializers.ModelSerializer):
+class SettingConfigSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = SettingConfig
         fields = ['user_id', 'table', 'guid', 'meta', 'platform', 'modified']
+
